f87b9817473d9e82a6748ea8093f2c8d94ce2304
Hanif8193 <hanifdostmuhammad8193@gmail.com>
2024-10-23 12:39:26 +0500

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,16 +3,16 @@ export default function About(){
       <div className="bg-blue-500 p-5 flex flex-col  h-screen">
         <h1 className="text-white text-center py-10 text-5xl italic font-bold"> About Us</h1>
         <p className="text-2xl italic text-white ">
-        Welcome to MH Services! We are a passionate team of dedicated front-end developers and creative 
-  freelancers based in Karachi, Pakistan. With a focus on crafting clean, responsive, and user-friendly web interfaces,
-   we help businesses transform their online presence.Our goal is toblend creativity with functionality,
-   ensuring that every project we deliver meets the highest standards of quality and performance. Whether it's building a custom website, 
-  optimizing an existing one, or delivering an interactive user experience, we're here to bring your vision to life.
-  At MH Services, we believe in collaboration and clear communication. We work closely with our clients 
-  to understand their needs and create solutions that drive results.
-  Let’s build something amazing together!
-  Feel free to adjust it to suit your style and company name MH Services! Would you like to modify or
-   expand on any part of it?
+       
+
+
+        I am a passionate front-end developer dedicated to building responsive and user-friendly websites,
+         focusing on clean code and delivering seamless user experiences.
+
+
+
+
+       
         </p>
       </div>
     )
